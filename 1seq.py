ade19d23769e56f846bb5569215c23a9a20c65e3
kvoloshenko <kvoloshenko@gmail.com>
2022-05-02 09:21:01 +0300

    Added to the project

---
+++ 1seq.py
@@ -0,0 +1,36 @@
+"""
+4. (МОДУЛЬ 1) Создать новый проект, в нем создать модуль 1seq.py. Задание:
+Пользователь вводит количество элементов будущего списка
+После этого по очереди по одной вводит любые цифры
+Сохранить цифры в список
+Отсортировать список по возрастанию и вывести на экран
+Пример работы: Введите количество элементов: 3
+Введите 1 элемент: 5
+Введите 2 элемент: 2
+Введите 3 элемент: 4
+Вывод: [2, 4, 5]
+
+
+"""
+l_numbers = []
+
+while True:
+    answer = input('Enter the number of items: ')
+    if answer.isdigit(): break
+
+items_num = int(answer)
+print('answer =', items_num)
+
+for i in range(1, items_num + 1):
+    while True:
+        s_input = 'Attempt=' + str(i) + ' Enter any number: '
+        s_num = input(s_input)
+        if s_num.isdigit(): break
+    i_num = int(s_num)
+    print('i_num=', i_num)
+    l_numbers.append(i_num)
+
+
+print(type(l_numbers), l_numbers)
+l_numbers.sort()
+print('Sorted list is: ', l_numbers)
